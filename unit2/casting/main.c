0c99edf04cdf58779bb26b601153d2d7769e0d8b
Mahmoud Ayman <160341295+Ma7moud-2ymann@users.noreply.github.com>
2024-02-22 10:01:18 +0200

    Add files via upload

---
+++ unit2/casting/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int a,b;
+    int minumum;
+    printf("enter a and b :");
+    scanf("%d %d",a b);
+    minumum =(a<b) ?a :b;
+    printf("minumum is equal to : %d".minumum);
+    return 0;
+}
